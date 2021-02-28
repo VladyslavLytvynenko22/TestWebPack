cf4cf4d8166deb9fafa93dea62d9d9eaefd9a367
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-28 23:48:49 +0200

    rework

--- src/components/PizzaImage/PizzaImage.js
+++ src/components/PizzaImage/PizzaImage.js
@@ -1,11 +1,12 @@
-import PizzaImage from './../../assets/pizza.jpg';
-import React from 'react'
-import classes from './PizzaImage.css';
+import React from 'react';
 
-export default function pizzaImage() {
-  return (
+import classes from './PizzaImage.css';
+import PizzaImage from '../../assets/pizza.jpg';
+
+const pizzaImage = (props) => (
     <div className={classes.PizzaImage}>
-      <img src={PizzaImage} className={PizzaImg}/>
+        <img src={PizzaImage} className={classes.PizzaImg} />
     </div>
-  )
-}
+);
+
+export default pizzaImage;
